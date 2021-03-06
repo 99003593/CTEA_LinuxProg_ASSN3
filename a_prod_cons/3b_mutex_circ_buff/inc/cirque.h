3f8dcb48471bd9c6a37b40ec2a0a75c28bc61c28
99003593 <gouthamm.gurusamy@ltts.com>
2021-03-06 10:12:00 +0530

    Add code for activity 3b
    producer consumer problem solution with semaphores and cirqular buffers
    but using mutexes where possible

---
+++ a_prod_cons/3b_mutex_circ_buff/inc/cirque.h
@@ -0,0 +1,24 @@
+#ifndef __CIRQUE_H_
+#define __CIRQUE_H_
+
+#include <stdbool.h>
+
+typedef struct cirque_s cirque_t;
+
+/**
+ * Ownership Contract:
+ *      Any element enqueued to cirque becomes owned by the cirque.
+ *      Any element dequeued from the cirque becomes owned by the user code.
+ *      Pass heap memory pointers as elements of cirque.
+ */
+
+cirque_t* cirque_create(int capacity);
+bool cirque_isfull(cirque_t* p_crq);
+bool cirque_isempty(cirque_t* p_crq);
+void cirque_enqueue(cirque_t* p_crq, void* p_item);
+int cirque_size(cirque_t* p_crq);
+void* cirque_peek(cirque_t* p_crq, int pos);
+void* cirque_dequeue(cirque_t* p_crq);
+void cirque_destroy(cirque_t *p_crq);
+
+#endif // __CIRQUE_H_
---
+++ a_prod_cons/3b_mutex_circ_buff/src/main.c
@@ -0,0 +1,106 @@
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "cirque.h"
+
+#define NUM_TRIES (10)
+
+cirque_t *p_crq;
+sem_t sync_prodcons;
+pthread_mutex_t mx_cirque;
+
+void print_cirque(const char *tag, cirque_t *p_crq)
+{
+    int* p_i;
+
+    printf("%s", tag);
+    for (int i = 0; i < cirque_size(p_crq); i++)
+    {
+        p_i = (int*)cirque_peek(p_crq, i);
+        if (p_i != NULL)
+            printf(" %d", *(p_i));
+        else
+            printf(" %c", '-');
+    }
+    printf("\n");
+}
+
+void *producer(void *pv)
+{
+    const struct timespec sl_time = {
+        .tv_sec = 0,
+        .tv_nsec = 50 * 1000 * 1000 // 50ms
+    };
+    int *p_t;
+    for (int i = 0; i < NUM_TRIES; i++)
+    {
+        // Sleep for a bit of time to simulate some work
+        nanosleep(&sl_time, NULL);
+        // Wait until resource is free
+        pthread_mutex_lock(&mx_cirque);
+
+        p_t = (int *)malloc(sizeof(int));
+        *p_t = i;
+        cirque_enqueue(p_crq, p_t);
+
+        print_cirque("[producer]", p_crq);
+
+        // Free resource
+        pthread_mutex_unlock(&mx_cirque);
+        // Signal to consumer that data is available
+        sem_post(&sync_prodcons);
+    }
+    //pthread_exit(NULL);
+}
+
+void *consumer(void *pv)
+{
+    const struct timespec sl_time = {
+        .tv_sec = 0,
+        .tv_nsec = 380 * 1000 * 1000 // 380ms
+    };
+    int *p_t;
+    for (int i = 1; i <= NUM_TRIES; i++)
+    {
+        // Wait until data is available from producer
+        sem_wait(&sync_prodcons);
+        // Wait until resource is free
+        pthread_mutex_lock(&mx_cirque);
+
+        p_t = (int *)cirque_dequeue(p_crq);
+
+        print_cirque("<consumer>", p_crq);
+
+        // Release data memory
+        free(p_t);
+
+        // Free resource
+        pthread_mutex_unlock(&mx_cirque);
+        // Sleep for a bit of time to simulate some work
+        nanosleep(&sl_time, NULL);
+    }
+    //pthread_exit(NULL);
+}
+int main()
+{
+    pthread_t th_p, th_c;
+    // Initialize semaphore for producer-consumer syncronization
+    sem_init(&sync_prodcons, 0, 0);
+    // Initialize semaphore for resource access
+    pthread_mutex_init(&mx_cirque, NULL);
+
+    // Initialize cirque
+    p_crq = cirque_create(NUM_TRIES);
+
+    pthread_create(&th_p, NULL, producer, NULL);
+    pthread_create(&th_c, NULL, consumer, NULL);
+    pthread_join(th_p, NULL);
+    pthread_join(th_c, NULL);
+
+    sem_destroy(&sync_prodcons);
+    pthread_mutex_destroy(&mx_cirque);
+    cirque_destroy(p_crq);
+    return 0; //exit(0);
+}
